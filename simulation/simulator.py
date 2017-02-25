34b2f83d420e2a0bee7e3588435c2579d8e99e8e
colinfd <colinfd@stanford.edu>
2017-02-24 23:12:55 -0800

    updated simulator

--- simulation/simulator.py
+++ simulation/simulator.py
@@ -8,17 +8,32 @@ class Simulator():
     Automatic simulator that manages cash flows and makes stock decisions based on 3 user-defined functions:
     val_fun, buy_fun, and sell_fun
     """
-    def __init__(self,sim_length,cash,val_fun,buy_fun,sell_fun,n_stocks=None,verbose=False):
+    def __init__(self,sim_length,cash,val_fun,buy_fun,sell_fun,n_stocks=None,verbose=False,buy_pt='open',sell_pt='open',dates=None,exclude_list=[],daily_limit = 1e10,data_path=None):
         self.sim_length = sim_length #days
         self.start_cash = cash #dollars
         self.val_fun = val_fun
         self.buy_fun = buy_fun
         self.sell_fun = sell_fun
         self.verbose= verbose
-
-        self.dates = [str(date).split('T')[0] for date in pickle.load(open('/home/colinfd/silly_money/examples/A.pkl')).index.values] #1771 dates for market trading
-        self.data_path = '/home/colinfd/silly_money/data/'
-        self.stock_names = [stock_pkl.split('/')[-1][:-4] for stock_pkl in glob.glob(self.data_path + '*.pkl')]
+        self.buy_pt = buy_pt
+        self.sell_pt = sell_pt
+        self.daily_limit = daily_limit
+        
+        if dates is None:
+            self.dates = [str(date).split('T')[0] for date in pickle.load(open('/home/colinfd/silly_money/examples/A.pkl')).index.values] #1771 dates for market trading
+        else:
+            self.dates = dates
+       
+        if data_path is None:
+            self.data_path = '/home/colinfd/silly_money/data/'
+        else:
+            self.data_path = data_path
+        
+        self.stock_names = []
+        for stock_pkl in glob.glob(self.data_path + '*.pkl'):
+            stock_name = stock_pkl.split('/')[-1][:-4]
+            if stock_name not in exclude_list:
+                self.stock_names.append(stock_name)
         if n_stocks is not None: self.stock_names = self.stock_names[:n_stocks+1]
 
         self.stock_data = {}
@@ -37,21 +52,21 @@ class Simulator():
             for stock_name in self.stock_names:
                     self.held_stocks[stock_name] = 0
             
-            self.market_prices = {}
+            self.buy_market_prices = {}
 
     def run(self):
         self.cleanup()
         f = open(self.logfile,'a')
+        prev_cash = 1e10
         for day_ind in range(self.start_ind,self.start_ind+self.sim_length):
             self.day_ind = day_ind
             
             #create dictionary of current market prices
             for i,stock_name in enumerate(self.stock_names):
-                self.market_prices[stock_name] = self.market_price(stock_name)
+                self.buy_market_prices[stock_name] = self.market_price(stock_name,self.buy_pt)
                             
             #start logging
-            log = "%s\nCash: %.2f\nHeld: %s\n"%(self.dates[day_ind],self.cash,self.stock_string(self.held_stocks)) #start log of new day
-            log += "Value: %.2f\n"%(self.total_value(self.held_stocks) + self.cash)
+            log = "%s\nCash: %.2f\nHeld: %s\n"%(self.dates[day_ind],self.cash,self.stock_string(self.held_stocks,pt='open')) #start log of new day
             
             #use functions to determine activity for the day
             val_dict = {}
@@ -62,27 +77,34 @@ class Simulator():
             for stock_name in sell_dict:
                 self.held_stocks[stock_name] -= sell_dict[stock_name]
                 assert self.held_stocks[stock_name] >= 0 ,"Bad sell_fun ... NEGATIVE STOCKS!"
-                self.cash += sell_dict[stock_name]*self.market_prices[stock_name]
+                self.cash += sell_dict[stock_name]*self.market_price(stock_name,self.sell_pt)
             
-            buy_dict = self.buy_fun(val_dict,self.cash,self.market_prices)
+            if self.cash/prev_cash > self.daily_limit: #exceeded maximum daily increase...avoid non-physical gains
+                self.cash = prev_cash
+            else:
+                prev_cash = self.cash
+            
+            log += "Value: %.2f\n"%self.cash
+            
+            buy_dict = self.buy_fun(val_dict,self.cash,self.buy_market_prices)
             for stock_name in buy_dict:
                 self.held_stocks[stock_name] += buy_dict[stock_name]
-                self.cash -= buy_dict[stock_name]*self.market_prices[stock_name]
+                self.cash -= buy_dict[stock_name]*self.buy_market_prices[stock_name]
                 assert self.cash > 0, "Bad buy_fun ... NEGATIVE CASH!"
 
-            log += "Sales: %s\n"%self.stock_string(sell_dict)
-            log += "Purchases: %s\n\n"%self.stock_string(buy_dict)
+            log += "Sales: %s\n"%self.stock_string(sell_dict,self.sell_pt)
+            log += "Purchases: %s\n\n"%self.stock_string(buy_dict,self.buy_pt)
             if self.verbose: print log
 
             f.write(log)
         print "Run finished"
         f.close()
 
-    def market_price(self,stock_name):
+    def market_price(self,stock_name,pt):
         i = -1
-        prices = self.read_stock(stock_name,start_i=0,end_i=self.day_ind+1)
+        prices = self.read_stock(stock_name,start_i=0,end_i=self.day_ind+1,pt=pt)
         while True:
-            if prices[i] is np.nan:
+            if "%s"%prices[i] == 'nan':
                 if i == -len(prices):
                     return 1e10 #stock has no history yet, do not allow purchase
                 else:
@@ -91,7 +113,7 @@ class Simulator():
                 assert type(prices[i]) == np.float16
                 return prices[i]
 
-    def stock_string(self,dict):
+    def stock_string(self,dict,pt):
         """
         Take stock dict (stock name keys with int values) and return string as "GOOGx4@122.54 ..." using current market prices
         """
@@ -99,19 +121,20 @@ class Simulator():
         keys = dict.keys()
         for stock_name in dict:
             if dict[stock_name] > 0:
-                out += "%sx%s@%.2f "%(stock_name,dict[stock_name],self.market_prices[stock_name])
+                out += "%sx%s@%.2f "%(stock_name,dict[stock_name],self.market_price(stock_name,pt))
         return out
 
     def total_value(self,held_stocks):
         """
+        DEPRECATED
         Take stock dict and return value at current market price
         """
         if len(held_stocks) == 0:
             return 0
-        total = sum([self.market_prices[stock_name]*held_stocks[stock_name] for stock_name in held_stocks if held_stocks[stock_name] > 0]) 
+        total = sum([self.market_price(stock_name,self.sell_pt)*held_stocks[stock_name] for stock_name in held_stocks if held_stocks[stock_name] > 0]) 
         return total
 
-    def read_stock(self,stock_name,start_i=0,end_i=None,pt='open'):
+    def read_stock(self,stock_name,start_i=0,end_i=None,pt='open',prev_day=False):
         """
         Return numpy array of slice of stock_data from start_i to end_i
         pt = 'open', 'close', 'high', 'low', 'volume'
@@ -121,7 +144,7 @@ class Simulator():
         if pt not in self.stock_data:
             self.stock_data[pt] = {}
             for stock in self.stock_names:
-                f = open(self.data_path + stock_name + '.pkl')
+                f = open(self.data_path + stock + '.pkl')
                 stock_dict = pickle.load(f)
                 f.close()
                 self.stock_data[pt][stock] = np.zeros(len(self.dates),dtype=np.float16)
@@ -134,5 +157,7 @@ class Simulator():
         
         if end_i == None:
             end_i = self.day_ind + 1
+        if prev_day:
+            end_i -= 1
 
         return self.stock_data[pt][stock_name][start_i:end_i]
